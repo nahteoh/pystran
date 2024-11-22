b45876041ea2211cf1b06ee8b3d32552a37a90a4
Petr Krysl <pkrysl@ucsd.edu>
2024-11-22 09:29:56 -0800

    return none

--- stranalyzer/model.py
+++ stranalyzer/model.py
@@ -1,8 +1,10 @@
+import numpy
 from numpy import array, zeros
 import stranalyzer.property
 
-def create():
+def create(dim = 2):
     m = dict()
+    m['dim'] = dim # Dimension of the model
     m['joints'] = dict()
     m['truss_members'] = dict()
     return m
@@ -15,7 +17,7 @@ def add_joint(m, identifier, coordinates):
         raise Exception("Joint already exists")
     else:
         m['joints'][identifier] = {'coordinates' : array(coordinates)}
-    return m
+    return None
         
 def add_truss_member(m, identifier, connectivity, properties):      
     """
@@ -24,8 +26,8 @@ def add_truss_member(m, identifier, connectivity, properties):
     if (identifier in m['truss_members']):
         raise Exception("Truss member already exists") 
     else:
-        m['truss_members'][identifier] = {'connectivity' : array(connectivity), 'properties' : properties}  
-    return m
+        m['truss_members'][identifier] = {'connectivity' : array(connectivity, dtype=numpy.int32), 'properties' : properties}  
+    return None
 
 def add_support(j, dir, value = 0.0):
     """
@@ -34,5 +36,25 @@ def add_support(j, dir, value = 0.0):
     if ('supports' not in j):
         j['supports'] = dict()
     j['supports'][dir] = value
-    return j
+    return None
+
+def number_dofs(m):
+    """
+    Number degrees of freedom.
+    """
+    n = 0
+    for j in m['joints'].values():
+        j['dof'] = array([i for i in range(m['dim'])], dtype=numpy.int32)
+        for d in range(2):
+            if ('supports' not in j or d not in j['supports']):
+                j['dof'][d] = n
+                n += 1
+    m['nfreedof'] = n
+    for j in m['joints'].values():
+        for d in range(2):
+            if ('supports' in j and d in j['supports']):
+                j['dof'][d] = n
+                n += 1
+    m['ntotaldof'] = n
+    return None
     
