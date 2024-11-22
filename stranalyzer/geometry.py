fcf2107a22daee124ad96abab273a218e88629ee
Petr Krysl <pkrysl@ucsd.edu>
2024-11-22 09:30:07 -0800

    add length

--- stranalyzer/geometry.py
+++ stranalyzer/geometry.py
@@ -1,7 +1,14 @@
-from numpy import diff
+import numpy 
+from numpy.linalg import norm
 
 def delt(xi, xj):
     """
     Compute oriented vector from the first to the second joint
     """
     return xj - xi
+
+def len(xi, xj):
+    """
+    Compute distance from the first to the second joint
+    """
+    return  norm(delt(xj - xi))
