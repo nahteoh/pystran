14e47797a25fc33752c4ae911aefc32d6d3da161
Petr Krysl <pkrysl@ucsd.edu>
2025-01-09 19:56:43 -0800

    add truss geometry

--- stranalyzer/truss.py
+++ stranalyzer/truss.py
@@ -6,6 +6,15 @@ from stranalyzer import geometry
 from numpy import array, dot, reshape, transpose, hstack, vstack, arange, outer, concatenate
 from numpy.linalg import norm
 
+def truss_member_geometry(i, j):
+    """
+    Compute truss geometry.
+    """
+    e_x = geometry.delt(i['coordinates'], j['coordinates'])
+    L = geometry.len(i['coordinates'], j['coordinates'])
+    e_x /= L
+    return e_x, L
+
 def stiffness(e_x, L, E, A):
     """
     Compute truss stiffness matrix.
@@ -25,15 +34,11 @@ def assemble_stiffness(Kg, member, i, j):
     """
     Assemble truss stiffness matrix.
     """
-    e_x = geometry.delt(i['coordinates'], j['coordinates'])
-    L = geometry.len(i['coordinates'], j['coordinates'])
-    e_x /= L
+    e_x, L = truss_member_geometry(i, j)
     properties = member['properties']
     E, A = properties['E'], properties['A']
     k = stiffness(e_x, L, E, A)
-    print(k)
     dof = concatenate([i['dof'], j['dof']])
-    print(dof)
     for r in arange(len(dof)):
         for c in arange(len(dof)):
             gr, gc = dof[r], dof[c]
