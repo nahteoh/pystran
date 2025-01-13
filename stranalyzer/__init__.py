05a0f40b8e438f43ad47de3850120e52711c5be1
Petr Krysl <pkrysl@ucsd.edu>
2025-01-12 17:54:24 -0800

    add beam

--- stranalyzer/__init__.py
+++ stranalyzer/__init__.py
@@ -7,13 +7,14 @@ Created on Sun Nov 17 19:21:25 2024
 @author: pkonl
 """
 # Define the __all__ variable
-__all__ = ["model", "property", "geometry", "truss", "plots"]
+__all__ = ["model", "property", "geometry", "truss", "beam", "plots"]
 
 # Import the submodules
 from . import model
 from . import property
 from . import geometry
 from . import truss
+from . import beam
 from . import plots
 
 
