0ba43d1b326b038bb762cb13c13fd1c8a6dfc652
Petr Krysl <pkrysl@ucsd.edu>
2025-01-17 16:02:32 -0800

    make spfr 1 work

--- examples/spfr_weaver_gere_1.py
+++ examples/spfr_weaver_gere_1.py
@@ -17,19 +17,20 @@ from pystran import plots
 from math import sqrt
 from numpy.linalg import norm
 
-L = 96.0 # inches
-E = 1e7 # psi
-G = 4e6 # psi
-A = 9.0 # in^2
-Iz = 80.0 # in^4
-Iy = 28.0 # in^4
-Ix = 64.0 # in^4
-J = Ix
-P = 5000.0 # lb
+# SI units
+L = 3.0 
+E = 200e9
+G = 80e9
+A = 0.01
+Iz = 1e-3
+Iy = 1e-3
+Ix = 2e-3
+J = Ix # Torsional constant
+P = 60000
 
 m = model.create(3)
 
-jA, jB, jC, jD, jE = 1, 2, 3, 4, 5
+jA, jB, jC, jD, jE = 3, 1, 2, 4, 5
 model.add_joint(m, jA, [0.0, 0.0, 0.0])
 model.add_joint(m, jB, [0.0, L, 0.0])
 model.add_joint(m, jC, [2*L, L, 0.0])
@@ -39,11 +40,13 @@ model.add_joint(m, jE, [L, L, 0.0])
 model.add_support(m['joints'][jA], model.CLAMPED)
 model.add_support(m['joints'][jD], model.CLAMPED)
 
-xz_vector = [0, 0, 1]
+xz_vector = [1, 0, 0]
 p1 = property.beam_property('property_1', E, G, A, Ix, Iy, Iz, J, xz_vector)
 model.add_beam_member(m, 1, [jA, jB], p1)
-model.add_beam_member(m, 2, [jE, jB], p1)
-model.add_beam_member(m, 3, [jE, jC], p1)
+xz_vector = [0, 1, 0]
+p2 = property.beam_property('property_1', E, G, A, Ix, Iy, Iz, J, xz_vector)
+model.add_beam_member(m, 2, [jE, jB], p2)
+model.add_beam_member(m, 3, [jE, jC], p2)
 model.add_beam_member(m, 4, [jC, jD], p1)
 
 model.add_load(m['joints'][jB], model.U1, 2*P)
@@ -60,24 +63,31 @@ print('Number of all degrees of freedom = ', m['ntotaldof'])
 
 model.solve(m)
 
-print([j['displacements'] for j in m['joints'].values()])
+for id in [jB, jC, jE]:
+    j = m['joints'][id]
+    print(id, j['displacements'])
 
 # print(m['K'][0:m['nfreedof'], 0:m['nfreedof']])
 
 # print(m['U'][0:m['nfreedof']])
 
 
-# if norm(b['displacements'] - [ 0.,  0., -0.02238452,  0.00419677,  0.00593197,0.]) > 1.e-5:
-#     raise ValueError('Displacement calculation error')
-# else:
-#     print('Displacement calculation OK')
+if norm(m['joints'][1]['displacements'] - [-8.59409726e-04, 5.77635277e-05, 5.00764459e-03, 2.39333188e-03, -1.62316861e-03, 6.81331291e-04]) > 1.e-5:
+    raise ValueError('Displacement calculation error')
+else:
+    print('Displacement calculation OK')
+
+if norm(m['joints'][2]['displacements'] - [-0.00117605, 0.00325316, 0.00525552, 0.00128843, 0.00172094, -0.00077147]) > 1.e-5:
+    raise ValueError('Displacement calculation error')
+else:
+    print('Displacement calculation OK')
 
 # print('Reference: ', [-0.02238452,  0.00419677,  0.00593197])
 
 plots.plot_setup(m)
 plots.plot_members(m)
 # plots.plot_member_numbers(m)
-plots.plot_deformations(m, 3.0)
+plots.plot_deformations(m, 100.0)
 # ax = plots.plot_shear_forces(m, scale=0.50e-3)
 # ax.set_title('Shear forces')
 plots.show(m)
