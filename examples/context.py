04d71353f626b222b8eadc27eca8297b3ff9b1f2
Petr Krysl <pkrysl@ucsd.edu>
2025-01-18 11:32:13 -0800

    improve context

--- examples/context.py
+++ examples/context.py
@@ -5,9 +5,6 @@ Mechanism for making python aware of the pystran package.
 import os
 import sys
 
-# This is an ugly hack. Jupyter notebook runs in a different path than plain python.
-sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("."))
 
 import pystran
