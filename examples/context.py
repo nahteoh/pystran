20f12418dacdca68a4730d0557e5898eb8ff9eca
Petr Krysl <pkrysl@ucsd.edu>
2025-01-18 09:10:28 -0800

    update context

--- examples/context.py
+++ examples/context.py
@@ -6,7 +6,8 @@ import os
 import sys
 
 # This is an ugly hack. Jupyter notebook runs in a different path than plain python.
-sys.path.insert(0, os.path.abspath("../pystran"))
-sys.path.insert(0, os.path.abspath("../../pystran"))
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
 
 import pystran
