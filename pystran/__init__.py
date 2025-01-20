82070ca117357eb35ab7d5009e545dc5ea6f7962
Petr Krysl <pkrysl@ucsd.edu>
2025-01-19 17:33:58 -0800

    add paz

--- pystran/__init__.py
+++ pystran/__init__.py
@@ -7,11 +7,21 @@ Created on Sun Nov 17 19:21:25 2024
 """
 
 # Define the __all__ variable
-__all__ = ["model", "section", "geometry", "assemble", "truss", "beam", "plots"]
+__all__ = [
+    "model",
+    "section",
+    "rotation",
+    "geometry",
+    "assemble",
+    "truss",
+    "beam",
+    "plots",
+]
 
 # Import the submodules
 from . import model
 from . import section
+from . import rotation
 from . import geometry
 from . import assemble
 from . import truss
