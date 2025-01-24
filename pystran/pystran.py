b2a1988760327ef162619006623993913a054665
Petr Krysl <pkrysl@ucsd.edu>
2025-01-24 14:36:38 -0800

    add top

---
+++ pystran/pystran.py
@@ -0,0 +1,10 @@
+"""
+pystran - Python package for structural analysis with trusses and beams 
+
+(C) 2025, Petr Krysl
+
+This package is intended for educational purposes only. Professional users may
+find it too bare bones.
+
+The approach is based on classical weighted residual formulation (Galerkin).
+"""
