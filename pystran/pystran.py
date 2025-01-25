45b10c14af305f0a5ae326507b93dce96cfce59f
Petr Krysl <pkrysl@ucsd.edu>
2025-01-25 10:02:18 -0800

    add link to book

--- pystran/pystran.py
+++ pystran/pystran.py
@@ -6,5 +6,7 @@ pystran - Python package for structural analysis with trusses and beams
 This package is intended for educational purposes only. Professional users may
 find it too bare bones.
 
-The approach is based on classical weighted residual formulation (Galerkin).
+The approach is based on classical weighted residual formulation (Galerkin). The
+formulations are derived in the "Finite element modeling with shells and beams"
+[book](http://hogwarts.ucsd.edu/~pkrysl/femstructures-book/).
 """
