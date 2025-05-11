74067049c51d2ec933c4b1e169a889c4fc04a513
Ethan Ho <ethanjho03@gmail.com>
2025-05-10 17:05:00 -0700

    first try

---
+++ homework/hw5q3a.py
@@ -0,0 +1,117 @@
+from numpy import dot
+from numpy.linalg import norm
+import context
+from pystran import model
+from pystran import section
+from pystran import freedoms
+from pystran import plots
+
+
+
+# Create a two dimensional (planar) model.
+m = model.create(2)
+
+# There are three joints. (in m)
+model.add_joint(m, 1, [0.0, 0.0])
+model.add_joint(m, 2, [20*12, 0.0])
+model.add_joint(m, 3, [35*12, 0.0])
+model.add_joint(m, 4, [50*12, 0.0])
+model.add_joint(m, 5, [70*12, 0.0])
+
+
+model.add_support(m["joints"][1], freedoms.U1)
+model.add_support(m["joints"][1], freedoms.U2)
+model.add_support(m["joints"][1], freedoms.UR3)
+model.add_support(m["joints"][3], freedoms.U2)
+model.add_support(m["joints"][5], freedoms.U1)
+model.add_support(m["joints"][5], freedoms.U2)
+model.add_support(m["joints"][5], freedoms.UR3)
+
+
+
+E = 29000 # ksi
+I1 = 300 # in^2
+I2  = 1.25*I1
+A1 =  71.1378660898# in^2
+A2 = 76.6309432394 # in^2 
+s1 = section.beam_2d_section("section_1", E, A1, I1)
+s2 = section.beam_2d_section("section_1", E, A2, I2)
+
+
+model.add_beam_member(m, 1, [1, 2], s2)
+model.add_beam_member(m, 2, [2, 3], s1)
+model.add_beam_member(m, 3, [3, 4], s1)
+model.add_beam_member(m, 4, [4, 5], s2)
+
+model.add_load(m["joints"][2], freedoms.U2, -50)
+model.add_load(m["joints"][4], freedoms.U2, -50)
+
+model.number_dofs(m)
+nt = m["ntotaldof"]
+nf = m["nfreedof"]
+print("Total Degrees of Freedom = ", nt)
+print("Free Degrees of Freedom = ", nf)
+
+
+
+model.solve_statics(m)
+
+# The stiffness matrix for the free degrees of freedom can be printed.
+print(m["K"][0:3, 0:3])
+
+# Here are the calculated free degrees of freedom:
+print(m["U"][0:3])
+
+
+
+
+# We can calculate the reactions at the supports. This is the manual approach
+# to that using the partitioning of the stiffness matrix and the displacement
+# vector.
+Kdf = m["K"][nf:nt, 0:nf]
+Kdd = m["K"][nf:nt, nf:nt]
+Uf = m["U"][0:nf]
+Ud = m["U"][nf:nt]
+
+print('Displacement:', Uf)
+# The reactions follow:
+R = dot(Kdf, Uf) + dot(Kdd, Ud)
+print("Reactions = ", R)
+
+# In order to understand moment and shear diagrams, we start by plotting the
+# geometry with the orientation of the local coordinate system on each beam.
+plots.setup(m)
+plots.plot_members(m)
+ax = plots.plot_member_orientation(m, 1.0)
+ax.set_title("Local coordinate systems (red -- local x, blue -- local z)")
+plots.show(m)
+
+
+# The deformed shape shows the curvatures of the beam.
+plots.setup(m)
+plots.plot_members(m)
+ax = plots.plot_deformations(m, 100.0)
+ax.set_title("Deformed shape (magnified 100 times)")
+plots.show(m)
+
+# The bending moment can be compared with the curvature of the beam.
+plots.setup(m)
+plots.plot_members(m)
+ax = plots.plot_bending_moments(m, scale=0.1)
+ax.set_title("Moments")
+plots.show(m)
+
+# The shear forces are the slopes of the moment diagram.
+plots.setup(m)
+plots.plot_members(m)
+ax = plots.plot_shear_forces(m)
+ax.set_title("Shear forces")
+plots.show(m)
+
+# The reaction forces can be plotted after the reactions have been computed.
+model.statics_reactions(m)
+plots.setup(m)
+plots.plot_members(m)
+ax = plots.plot_reaction_forces(m)
+ax.set_title("Reaction forces")
+plots.show(m)
